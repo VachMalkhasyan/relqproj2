7748a7369f792c3c567266217068cbe465b85384
VachMalkhasyan <vachmalkhasyan@gmail.com>
2022-08-03 12:26:04 +0400

    03.08

--- imsarqacjs.js
+++ imsarqacjs.js
@@ -1,22 +1,73 @@
-let first =''
-let second=''
-let sign=''
-const numbers=['0','1','2','3','4','5','6','7','8','9']
-const action=['+','-','*','/']
+let a='';
+let b='';
+let sign='';
+let finish= false ;
+let havasar='=';
+
+const digit=['0','1','2','3','4','5','6','7','8','9']
+const action=['+','-','x','/']
+
+
 const out=document.getElementById('screen')
+
+
 function clearAll(){
-out.textContent=0
+a='';
+b='';
+sign='';
+finish=false;
+out.textContent=0;
 }
+//document.getElementById('clean').onclick=clearAll()
+
+//knopkeqi sxmelu pah
 document.getElementById('everything').onclick=(event) =>{
     const key =event.target.textContent
-    if(numbers.includes(key)){
-        first+=key;
-        out.textContent=first
-    }
+    
+    if(digit.includes(key)){
+        if(b === ''  && sign === ''){
 
+        a+=key;
+        out.textContent=a}
+        else if(a!=='' && b =='' && finish){
+            b=key
+            finish=false
+            out.textContent=b
+            console.log(b)
+        }    
+        else {
+            b+=key;
+            out.textContent=b;
+        }
+
+    }
+    
     if(action.includes(key)){
-        sign+=key;
+        sign += key;
         out.textContent=sign;
+        console.log(sign)
         return
     }
+}
+if(key === 'havasar'){
+    switch (sign) {
+        case '+':
+          out = a + b;
+          break;
+        case '-':
+          out = a - b;
+          break;
+        case 'x':
+          out = a * b;
+          break;
+        case '/':
+          if (b) {
+            out = a / b;
+          } else {
+            out= '0 mi gri';
+          }
+          break;
+        
+      
+      }
 }
